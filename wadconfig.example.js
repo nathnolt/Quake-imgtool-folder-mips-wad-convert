7643b599b82695a068b10eb24db2ccec1c576de2
nathnolt <nathnolt@gmail.com>
2022-01-09 00:08:11 +0100

    Add files via upload

---
+++ wadconfig.example.js
@@ -0,0 +1,30 @@
+// These are the custom overwrite settings for this wad.
+module.exports = {
+	relativeOutputWadDir: './subfolder/',
+	// forceRebuilt: true,
+	
+	// NOT YET SUPPORTED:
+	/*
+	Full bright
+	*/
+	
+	// fullbrights: true,
+	/* add this for exeptions when fullbrights is false */
+	// fullbrightTextures: [
+	// 	'texture1',
+	// 	'#slime3'
+	// ],
+	
+	/* add this for exeptions when fullbrights is true */
+	// noFullbrightTextures: [
+	// 	'texture1',
+	// 	'#slime3'
+	// ],
+	
+	
+	/*
+	Dither
+	*/
+	// ditherAlgorithm: 'Atkinson',
+	// ditherTextures: {},
+}
